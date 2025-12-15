f9d3b3c22266efb792aa08fe7b28a437235befa0
inspire12 <ox4443@naver.com>
2025-12-15 18:12:38 +0900

    feat: start 세팅

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +6,7 @@ app = FastAPI()
 @app.get("/hello")
 def hello():
     return {"message": "Hello FastAPI!"}
+
+if __name__ == '__main__':
+    print('Hi uv')
+    uvicorn.run(app, host='0.0.0.0', port=8880)
